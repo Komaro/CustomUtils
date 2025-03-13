22a1914e5928b3bad99d9d59969d2793e4b1b6d8
komaro <komaro318@gmail.com>
2025-03-13 20:34:41 +0900

    Service 초기화 버그 수정

---
+++ Assets/CustomUtils/Service/Core/Service.Editor.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+
+public static partial class Service {
+    
+    [InitializeOnLoadMethod]
+    private static void InitializeOnLoad() {
+        if (EditorApplication.isPlayingOrWillChangePlaymode == false) {
+            Initialize();
+        }
+    }
+}
+
+#endif

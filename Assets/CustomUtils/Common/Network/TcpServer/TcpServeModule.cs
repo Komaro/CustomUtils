7afa2cef972f0b818fc8e3b072c4f0a72d205cc3
komaro <komaro318@gmail.com>
2025-07-01 18:34:53 +0900

    실험 코드 작성

--- Assets/CustomUtils/Common/Network/TcpServer/TcpServeModule.cs
+++ Assets/CustomUtils/Common/Network/TcpServer/TcpServeModule.cs
@@ -268,60 +268,135 @@ public abstract class TcpServeModule<THeader, TData> : ITcpServeModule, ITcpRece
 
 public abstract class TcpFixServeModule<THeader, TData> : TcpServeModule<THeader, TData> {
 
-    protected RecyclableMemoryStreamManager memoryStreamManager = new();
+    protected readonly Channel<MemoryStream> memoryChannel = Channel.CreateUnbounded<MemoryStream>();
+    protected readonly RecyclableMemoryStreamManager memoryStreamManager = new();
+
+    protected readonly MemoryStream bufferStream;
+    
+    private static readonly int TCP_HEADER_SIZE = Marshal.SizeOf<TcpHeader>();
+
+    public TcpFixServeModule() => bufferStream = memoryStreamManager.GetStream();
+
+    protected override async Task<bool> ConnectAsync(TcpClient client, CancellationToken token) {
+        try {
+            var connectCount = 0;
+            var maxReconnectCount = 5;
+            while (token.IsCancellationRequested == false && client.Connected && connectCount < maxReconnectCount) {
+                try {
+                    var session = await ConnectSessionAsync(client, token);
+                    connectCount++;
+                    if (session == null) {
+                        continue;
+                    }
+
+                    _ = Task.Run(async () => {
+                        await ReceiveAsync(session, token);
+                        await ParseAsync(token);
+                    }, token);
+                } catch (DisconnectSessionException) {
+                    continue;
+                } catch (InvalidCastException) {
+                    continue;
+                }
+
+                return true;
+            }
+        } catch (SocketException ex) {
+            Logger.TraceError($"{ex.SocketErrorCode.ToString()} || {ex.Message}");
+        } catch(Exception ex){
+            Logger.TraceLog(ex);
+            client.Close();
+            return false;
+        }
+        
+        return false;
+    }
 
     protected override async Task ReceiveAsync(TcpSession session, CancellationToken token) {
         Logger.TraceLog($"Start {nameof(ReceiveAsync)} {nameof(Task)}", Color.LightGreen);
-        using (var memoryStream = memoryStreamManager.GetStream("module", RECEIVE_BUFFER_SIZE))
         using (var owner = memoryPool.Rent(RECEIVE_BUFFER_SIZE)) {
+            var buffer = owner.Memory;
             while (session.Connected && token.IsCancellationRequested == false) {
                 try {
-                    var buffer = owner.Memory;
                     var byteLength = await session.Stream.ReadAsync(buffer, token);
-                    await session.Stream.CopyToAsync(memoryStream, token);
                     if (byteLength == 0) {
                         throw new DisconnectSessionException();
                     }
 
-                    var readMemory = await memoryStream.ReadAsync(buffer[..byteLength], token);
-                    // TODO. Read Header
-                    var headerSize = Marshal.SizeOf<TcpHeader>();
-                    
+                    var stream = memoryStreamManager.GetStream("chunk");
+                    _ = await stream.ReadAsync(buffer[..byteLength], token);
+                    await memoryChannel.Writer.WriteAsync(stream, token);
                 } catch (DisconnectSessionException) {
                     Disconnect(session.ID);
                 } catch (SocketException) {
                     Disconnect(session.ID);
                 } catch (Exception ex) {
                     Logger.TraceLog(ex.Message);
+                } finally {
+                    memoryChannel.Writer.Complete();
                 }
             }
         }
     }
 
-    /*
-    public override Task<THeader> ReceiveHeaderAsync(TcpSession session, CancellationToken token) {
-        
-    }
-    */
+    protected virtual async Task ParseAsync(CancellationToken token) {
+        while (token.IsCancellationRequested == false) {
+            using var stream = await memoryChannel.Reader.ReadAsync(token);
+            stream.Position = 0;
+            await stream.CopyToAsync(bufferStream, token);
+            bufferStream.Position = 0;
+                
+            // TODO. HEADER, BODY 파싱 처리
+            // TODO. 메소드 추출 필요
+            if (bufferStream.Length - bufferStream.Position > TCP_HEADER_SIZE) {
+                Memory<byte> headerBytes = new byte[TCP_HEADER_SIZE];
+                var length = await bufferStream.ReadAsync(headerBytes, token);
 
+                if (bufferStream.Length - bufferStream.Position > length) {
+                    // TODO. BODY 처리
+                } else {
+                    bufferStream.Position -= TCP_HEADER_SIZE;
+                }
+            }
+            
+            // TODO. 읽고 남은 byte 처리
+            // TODO. 메소드 추출 필요
+            var remainBytes = stream.Length - stream.Position;
+            if (remainBytes <= 0) {
+                bufferStream.SetLength(0);
+                bufferStream.Position = 0;
+                continue;
+            }
+
+            var buffer = bufferStream.GetBuffer();
+            Buffer.BlockCopy(buffer, (int)bufferStream.Position, buffer, 0, (int)remainBytes);
+            bufferStream.SetLength(remainBytes);
+            bufferStream.Position = 0;
+            
+            await stream.DisposeAsync();
+        }
+    }
+
+    // TODO. NetworkStream을 직접 읽어들이지 않고 MemoryStream에서 읽어들이도록 시도 
     protected override async Task<byte[]> ReadBytesAsync(NetworkStream stream, int length, CancellationToken token) {
-        using (var owner = MemoryPool<byte>.Shared.Rent(1024))
-        using (var memoryStream = memoryStreamManager.GetStream("module", 1024)) {
+        using (var owner = memoryPool.Rent(length))
+        using (var memoryStream = memoryStreamManager.GetStream("buffer", 1024)) {
+            bufferStream.Position = 0;
             var buffer = owner.Memory;
             var totalBytesLength = 0;
             while (totalBytesLength < length) {
-                var bytesLength = await stream.ReadAsync(buffer, token);
-                if (bytesLength == 0) {
+                var readBytes = await bufferStream.ReadAsync(buffer, token);
+                if (readBytes <= 0) {
                     throw new DisconnectSessionException();
                 }
-
-                token.ThrowIfCancellationRequested();
-
-                await memoryStream.WriteAsync(buffer[..bytesLength], token);
-                totalBytesLength += bytesLength;
+                
+                await memoryStream.WriteAsync(buffer[..readBytes], token);
+                totalBytesLength += readBytes;
             }
-
-            return memoryStream.GetBuffer();
+            
+            
         }
+
+        throw new NotImplementedException();
     }
 }
